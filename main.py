8c20b8470a2cf22b4dbf6442cf1c084fa0540319
ad <ad>
2021-04-30 19:25:33 +0300

    hash_object function added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import hashlib
+import zlib
 
 # Initializing a git repository
 def init(repo):
@@ -11,5 +13,25 @@ def init(repo):
         f.write(b'ref:refs/heads/master')
     print(f'initialized empty repository: {repo}')
 
+# Hashing objects
+def hash_object(data, obj_type, write=True):
+    ''' Compute has of object data of a given type and write to object
+    store if "write" is True. Return SHA-1 object hash as hex string.
+    '''
+    header = f"{obj_type} {len(data)}".encode()
+    full_data = header+b'\x00'+data
+    sha1 = hashlib.sha1(full_data).hexdigest()
+    print(sha1)
+    if write:
+        path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            with open(path, 'wb') as f:
+                f.write(zlib.compress(full_data))
+    return sha1
+
+
 if __name__ == '__main__':
     init('my-sample-repo')
+    os.chdir('my-sample-repo')
+    hash_object(b'test data', 'object type', True)
